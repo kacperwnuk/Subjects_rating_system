574c59c7c4e25f2d4b2b6920188a76f06b750610
kacperwnuk <kacperwnuk97@o2.pl>
2019-09-10 12:38:05 +0200

    Heroku preparation

--- subjects_rating_system/settings.py
+++ subjects_rating_system/settings.py
@@ -132,3 +132,8 @@ EMAIL_PORT = os.environ.get("EMAIL_PORT")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
+
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
